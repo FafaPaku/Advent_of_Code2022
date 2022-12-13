0559d02b3832255e435bb96f3f955f2b72a4e6dd
Fafa Paku <npaku1@jhu.edu>
2022-12-13 11:59:58 -0500

    Day 13 Commit
    committing solution

---
+++ day13/solution.py
@@ -0,0 +1,102 @@
+import functools
+
+
+# def mycmp(right, left):
+#     print("Compare ", left, " vs ", right)
+#     if left < right:
+#         return 1
+#     elif left > right:
+#         return -1
+#     else:
+#         return 0
+
+
+# print(min([3, 5], key=functools.cmp_to_key(mycmp)))
+# print(max([3, 5], key=functools.cmp_to_key(mycmp)))
+# print(min([1, 1], key=functools.cmp_to_key(mycmp)))
+
+# def packet_cmp(a: list[Packet], b: list[Packet]) -> int:
+#   result = 0
+
+#   for i in range(min(len(a), len(b))):
+#     match isinstance(a[i], list), isinstance(b[i], list):
+#       case True, True: result = packet_cmp(a[i], b[i])
+#       case True, False: result = packet_cmp(a[i], [b[i]])
+#       case False, True: result = packet_cmp([a[i]], b[i])
+#       case _: result = (a[i] > b[i]) - (a[i] < b[i])
+
+#     if result:
+#       return result
+
+#   return (len(a) > len(b)) - (len(a) < len(b))
+
+def GetInputList(path):
+    data = []
+    with open(path) as file:
+
+        for line in file:
+            array = line.replace("\n", "")
+            data.append(array)
+        return data
+
+
+def CompareHelp(left, right):
+    if (left == right):
+        return 0
+    if (left < right):
+        return -1
+    return 1
+
+
+def ComparePackets(left, right):
+    result = 0
+    for i in range(min(len(left), len(right))):                             
+        match isinstance(left[i], list), isinstance(right[i], list):        
+            case True, True: result = ComparePackets(left[i], right[i])     #If both values are lists
+            case True, False: result = ComparePackets(left[i], [right[i]])  #If exactly one value is an integer, convert the integer to a list
+            case False, True: result = ComparePackets([left[i]], right[i])  #If exactly one value is an integer, convert the integer to a list
+            case _: result = CompareHelp(left[i], right[i])                 #If both values are integers, the lower integer should come first
+
+        if result: #If equivalent, 0, skip... we want to coninue
+            return result
+
+    return CompareHelp(len(left), len(right))                               #If the left list runs out of items first we are good
+
+
+def GetSumRightOrder(input):
+    sum = 0
+    idx = 1
+    for i in range(len(input)):
+        if (i % 3 == 0):
+            left = eval(input[i])
+            right = eval(input[i+1])
+            print('Index', idx)
+            print('Compare %s vs %s' % (input[i], input[i+1]))
+            result = ComparePackets(left, right)
+            if (result == -1):
+                sum += idx
+            idx += 1
+            print('Result: ', result)
+
+    print('Sum of right order:', sum)
+
+
+input = GetInputList('day13/input.txt')
+GetSumRightOrder(input)
+
+input2 = []
+for item in input:
+    if item != '':
+        input2.append(eval(item))
+
+input2.append([[2]])
+input2.append([[6]])
+
+#https://www.geeksforgeeks.org/how-does-the-functools-cmp_to_key-function-works-in-python/
+result = sorted(input2, key=functools.cmp_to_key(ComparePackets))
+second = result.index([[2]]) + 1
+sixth = result.index([[6]]) +1
+
+print('Part2: ',second * sixth )
+
+print(ComparePackets([1], [1]))
